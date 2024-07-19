001d95eb2f9b72d9ff5af7bb592eb2dc9959636b
Benji Wallis <benji@benjiwallis.xyz>
2024-07-20 00:36:04 +1200

    resolved bauild errors?

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,6 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { Logo } from "@/components/logo";
 import { motion } from "framer-motion";
 
